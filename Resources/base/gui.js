10dac73555a3724a5ff21a92836cc29a69d8f9c1
Mahmut Bulut <mahmutbulut0@gmail.com>
2013-10-04 23:22:36 +0300

    Ana arayüz düzenlemeleri
    Signed-off-by: Mahmut Bulut <mahmutbulut0@gmail.com>

--- Resources/base/gui.js
+++ Resources/base/gui.js
@@ -1,6 +1,25 @@
 
-// editor load
+// Editor Load 
+// SİLME!
+
+function editorheight () {
+	var heig = $( window ).height();
+	$( '#editor-container' ).height( heig - 56 );
+}
+
+editorheight();
+
+$( window ).resize(function() {
+	editorheight();
+});
+
+
+
+ $().ready(function(){
+   $("#main").splitter();
+ });
 
 function loadNPM(){
 	$( "#content" ).load( "./npm.html" );
 }
+
