e66c2a193eb11808a14ea0f65a7869c83ae22ff2
Mahmut Bulut <mahmutbulut0@gmail.com>
2013-10-05 03:14:51 +0300

    Introducing to TreeView
    Signed-off-by: Mahmut Bulut <mahmutbulut0@gmail.com>

--- Resources/base/tree.js
+++ Resources/base/tree.js
@@ -1,3 +1,4 @@
+/*
 $(function () {
 	// TO CREATE AN INSTANCE
 	// select the tree container using jQuery
@@ -5,30 +6,41 @@ $(function () {
 		// call `.jstree` with the options object
 		.jstree({
 			// the `plugins` array allows you to configure the active plugins on this instance
-			"plugins" : ["themes","html_data","ui","crrm","hotkeys"],
+			"plugins" : ["themes","json_data","ui","crrm","hotkeys", "dnd", "contextmenu", "types"],
 			// each plugin you have included can have its own config object
-			"core" : { "initially_open" : [ "phtml_1" ] }
-			// it makes sense to configure a plugin only if overriding the defaults
-		})
-		// EVENTS
-		// each instance triggers its own events - to process those listen on the container
-		// all events are in the `.jstree` namespace
-		// so listen for `function_name`.`jstree` - you can function names from the docs
-		.bind("loaded.jstree", function (event, data) {
-			// you get two params - event & data - check the core docs for a detailed description
-		});
-	// INSTANCES
-	// 1) you can call most functions just by selecting the container and calling `.jstree("func",`
-	setTimeout(function () { $("#treeview").jstree("set_focus"); }, 500);
-	// with the methods below you can call even private functions (prefixed with `_`)
-	// 2) you can get the focused instance using `$.jstree._focused()`. 
-	setTimeout(function () { $.jstree._focused().select_node("#phtml_1"); }, 1000);
-	// 3) you can use $.jstree._reference - just pass the container, a node inside it, or a selector
-	setTimeout(function () { $.jstree._reference("#phtml_1").close_node("#phtml_1"); }, 1500);
-	// 4) when you are working with an event you can use a shortcut
-	$("#treeview").bind("open_node.jstree", function (e, data) {
-		// data.inst is the instance which triggered this event
-		data.inst.select_node("#phtml_2", true);
+*/
+
+function setTree (tree) {
+	$(function () {
+		$("#treeview").jstree({ 
+			"plugins" : ["themes","json_data","ui","crrm", "dnd", "contextmenu", "types"],
+			//Themes sections
+			"themes" : {
+				"theme" : "apple",
+				"dots" : true,
+				"icons" : true
+			},
+			//Data section
+			"json_data" : {
+				"data" : tree
+			}
+		}).bind("select_node.jstree", function (e, data) { alert(data.rslt.obj.data("id")); });
 	});
-	setTimeout(function () { $.jstree._reference("#phtml_1").open_node("#phtml_1"); }, 2500);
-});
+}
+
+var data = [
+					{ 
+						"data" : "arrayUS node", 
+						"metadata" : { id : 23 },
+						"children" : [ "Child andrud 1", "A Child 2" ]
+					},
+					{ 
+						"attr" : { "id" : "li.node.id1" }, 
+						"data" : { 
+							"title" : "Long format demo", 
+							"attr" : { "href" : "#" } 
+						} 
+					}
+			];
+
+setTree(data);
