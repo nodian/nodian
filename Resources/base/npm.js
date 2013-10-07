7a31a5506114176efb1de5dbc3d025c8e27e742e
Mahmut Bulut <mahmutbulut0@gmail.com>
2013-10-08 01:17:24 +0300

    npm fetch

--- Resources/base/npm.js
+++ Resources/base/npm.js
@@ -1,5 +1,21 @@
 
 function getPackageJson (startKeyword, endKeyword, grouplevel) {
-	$.getJSON();
-	_.each();
+	function() {
+		var kafa = "http://registry.npmjs.org/-/_view/byKeyword?startkey=[\"";
+		var govde = "\"]&endkey=[\"";
+		var ayak = "\",{}]&group_level=";
+		$.getJSON( kafa+startKeyword+govde+endKeyword+ayak+grouplevel, {
+			tags: "mount rainier",
+			tagmode: "any",
+			format: "json"
+		})
+		.done(function( data ) {
+			$.each( data.items, function( i, item ) {
+				$( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+				if ( i === 3 ) {
+					return false;
+				}
+			});
+		});
+	}
 }
